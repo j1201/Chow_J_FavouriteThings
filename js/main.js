7ab8ddb67db0752e201dc43408fec934ff1aed0d
Joanna C <joannachowhy@gmail.com>
2022-10-14 15:36:35 -0400

    update gs animation setting

--- js/main.js
+++ js/main.js
@@ -4,14 +4,24 @@ import {getData} from "./modules/dataMiner.js";
     let theTemplate = document.querySelector("#fav-template").content,
         theThing = document.querySelector(".fav-section"),
         btnContainer = document.querySelectorAll(".things"), // list of three items
+        arrow = document.querySelectorAll(".arrow"),
         buttonClicked; // get target button
     
-    // add green sock library to create ease-in-out animation
+    // add green sock library to create ease-in-out animation for text content
     const timeline = gsap.timeline({ default: { duration: 1}});
 
     timeline
-    .from('.title', { opacity: 0, y: "50%", ease: "power1.out" })
-    .from('.things', { opacity: 0, y: "50%", ease: "power1.out", stagger: .5 })
+        .from('.title', { opacity: 0, y: "50%", ease: "power1.out" })
+        .from('.things', { opacity: 0, y: "50%", ease: "power1.out", stagger: .5 })
+
+    // set arrow direction
+    gsap.to(arrow,{rotation: -45, transformOrigin:"center"} )
+
+    // rotate arrow direction with GS library
+    function arrowRotate () { gsap.to(this.lastElementChild.childNodes[1],{rotation: 0, transformOrigin:"center"} )}
+
+    // arrow return to starting rotation setting
+    function arrowReturn() { gsap.to(arrow,{rotation: -45, transformOrigin:"center"} )}
 
     // retrieve content from different item
     function retrieveThing (event) {
@@ -42,8 +52,12 @@ import {getData} from "./modules/dataMiner.js";
 
     }
 
-   
+    // animate arrow rotation with mouseover event
+    btnContainer.forEach(arrowEl => {
+        arrowEl.addEventListener('mouseover', (arrowRotate))
+        arrowEl.addEventListener("mouseleave", (arrowReturn))
+    })
 
-    
+    // get data with click event
     btnContainer.forEach(addEventListener("click", retrieveThing));// add click event to each button
 })();
